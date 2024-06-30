2c791331ba9cc81fcc23c7fb0ef737d94b267cd0
dr-saad-la <dr.saad.laouadi@gmail.com>
2024-06-30 19:06:04 +0100

    Clean artifacts

--- rustapps/advanced_calculator/target/debug/deps/integration_test-12642f5bc25570d8.d
+++
@@ -1,5 +0,0 @@
-/Users/daas/RustProgramming/rust-distilled/rustapps/advanced_calculator/target/debug/deps/integration_test-12642f5bc25570d8: tests/integration_test.rs
-
-/Users/daas/RustProgramming/rust-distilled/rustapps/advanced_calculator/target/debug/deps/integration_test-12642f5bc25570d8.d: tests/integration_test.rs
-
-tests/integration_test.rs:
--- rustapps/advanced_calculator/target/debug/deps/integration_test-9c62c3891f8f87af.d
+++
@@ -1,5 +0,0 @@
-/Users/daas/RustProgramming/rust-distilled/rustapps/advanced_calculator/target/debug/deps/libintegration_test-9c62c3891f8f87af.rmeta: tests/integration_test.rs
-
-/Users/daas/RustProgramming/rust-distilled/rustapps/advanced_calculator/target/debug/deps/integration_test-9c62c3891f8f87af.d: tests/integration_test.rs
-
-tests/integration_test.rs:
