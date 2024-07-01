bed519b550f961832cd3fbdc42a1412d7fc1fc4c
dr-saad-la <dr.saad.laouadi@gmail.com>
2024-07-01 14:00:42 +0100

    Edit the test file to test newly added functions

--- rustapps/advanced_calculator/tests/integration_test.rs
+++ rustapps/advanced_calculator/tests/integration_test.rs
@@ -1,5 +1,7 @@
 use advanced_calculator::calc::{add, divide, multiply, subtract};
+use advanced_calculator::calc::{complex, exp, hyperbolic};
 use advanced_calculator::calc::{log, power, sqrt, trig};
+use num_complex::Complex;
 
 #[test]
 fn test_add() {
@@ -46,4 +48,24 @@ fn test_trig() {
 fn test_log() {
     assert_eq!(log::ln(1.0), 0.0);
     assert_eq!(log::log10(100.0), 2.0);
+}
+
+#[test]
+fn test_exp() {
+    assert_eq!(exp::exp(1.0), (1.0 as f64).exp());
+}
+
+#[test]
+fn test_hyperbolic() {
+    assert_eq!(hyperbolic::sinh(1.0), (1.0 as f64).sinh());
+    assert_eq!(hyperbolic::cosh(1.0), (1.0 as f64).cosh());
+    assert_eq!(hyperbolic::tanh(1.0), (1.0 as f64).tanh());
+}
+
+#[test]
+fn test_complex() {
+    let a = Complex::new(1.0, 2.0);
+    let b = Complex::new(3.0, 4.0);
+    let result = complex::add_complex(a, b);
+    assert_eq!(result, Complex::new(4.0, 6.0));
 }
