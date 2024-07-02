335cb3f4a08af6baf17355f91c1ab0853777eb44
dr-saad-la <dr.saad.laouadi@gmail.com>
2024-07-02 16:52:47 +0100

    Add more examples in main file

--- code/03_control_flow/if_statement/src/main.rs
+++ code/03_control_flow/if_statement/src/main.rs
@@ -23,10 +23,38 @@ fn simple_if_statement() {
 // }
 
 // Warning code
-
 fn if_with_parentheses() {
     let num = 7;
+    #[allow(unused_parens)]
     if (num > 0) {
         println!("{} is positive number", num);
+    }
+}
+
+// another example
+
+fn with_without_parens() {
+    let number = 5;
+
+    // Both styles are valid
+    if number > 0 {
+        println!("No parentheses needed.");
+    }
+
+    // You will get warning here
+    // you can suppress the warning though
+    #[allow(unused_parens)]
+    if (number > 0) {
+        println!("Parentheses are optional.");
+    }
+}
+
+// Erroneous code, non-boolean-evaluation
+
+fn non_bool_eval() {
+    let num = 7;
+
+    if num {
+        println!("{} is positive.", num);
     }
 }
